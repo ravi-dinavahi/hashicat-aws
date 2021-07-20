9611fd01eff1a0d5010c2ae73f1c9aca1a14b0d9
Ravi Dinavahi <rpdinavahi@gmail.com>
2021-07-20 18:29:30 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,4 +2,9 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
   # insert the 5 required variables here
+  acceleration_status
+  bucket
+  bucket_prefix = "ravi-dinavahi"
+  polciy
+  request_payer
 }
